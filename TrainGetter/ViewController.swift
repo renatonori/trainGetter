ac342fcf7f66a807d12282045f571eb873bffe79
Renato Ioshida <renatoioshida@MacBook-Pro-de-Renato.local>
2017-09-16 19:54:46 -0300

    Initial Commit

---
+++ TrainGetter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrainGetter
+//
+//  Created by Renato Ioshida on 16/09/17.
+//  Copyright © 2017 Renato Ioshida. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
